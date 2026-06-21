32767c3dfac617f60d5ddb8fa1bde9c9808654e4
rodrigo <kakakpegado@hotmail.com>
2026-06-20 21:27:35 -0300

    feat: configuracao inicial do FastAPI

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI, APIRouter
+
+app = FastAPI()
+router = APIRouter()
+
+@router.get('/')
+def first():
+    return 'Ola mundo, Hello world'
+
+
+app.include_router(router)    
